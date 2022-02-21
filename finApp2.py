4323990fccc377369707a0d2a0de7234c120d111
OscarChandraQUT <92902350+OscarChandraQUT@users.noreply.github.com>
2022-02-21 13:18:39 +1000

    Add files via upload
    scrape volume and 1 year estimate

--- finApp2.py
+++ finApp2.py
@@ -10,28 +10,56 @@ from bs4 import BeautifulSoup
 def web_content_div(web_content, class_path):
     web_content_div = web_content.find_all('div', {'class': class_path})
     try:
-        spans = web_content_div[0].find_all('span')
+        spans = web_content_div[0].find_all('fin-streamer')
         texts = [span.get_text() for span in spans]
+        #print('texts: ' + str(texts))
     except IndexError:
         texts = []
     return texts
 
+def one_year_est(web_content, class_path): #different function because '1y est' is different tag
+    one_year_est = web_content.find_all('div', {'class': class_path})
+    try:
+        infos = one_year_est[0].find_all('td')
+        texts = [info.get_text() for info in infos]
+        #print(texts)
+    except IndexError:
+        texts = []
+    return texts
 
 def real_time_price(stock_code):  # get info from url
-    url = 'https://coinmarketcap.com/currencies/' + stock_code + '/'
+    url = 'https://finance.yahoo.com/quote/' + stock_code + '?p=' + stock_code
     try:
         r = requests.get(url)
         web_content = BeautifulSoup(r.text, 'lxml')
         texts = web_content_div(
-            web_content, "sc-16r8icm-0 kjciSH priceTitle")  # div class containing price and % change in span
+            web_content, "D(ib) Va(m) Maw(65%) Ov(h)")  # div class containing price and % change in 'fin-streamer' tag
         if texts != []:
-            price, change = texts[0], texts[1]
+            price, change, percent = texts[0], texts[1], texts[2]
         else:
-            price, change = ['empty'], ['empty']
+            price, change, percent = ['empty'], ['empty'], ['percent']
+        #finding volume
+        texts = web_content_div(web_content, 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)') #class containing volume
+        if texts !=[]:
+            volume = texts[0]
+        else:
+            volume = []
+        #find one year target
+        texts = one_year_est(web_content, 'D(ib) W(1/2) Bxz(bb) Pstart(12px) Va(t) ie-7_D(i) ie-7_Pos(a) smartphone_D(b) smartphone_W(100%) smartphone_Pstart(0px) smartphone_BdB smartphone_Bdc($seperatorColor)') #class containing one year target estimate
+        if texts !=[]:
+            for count, target in enumerate(texts):
+                if target == '1y Target Est': #find relevant 'td' tag
+                    one_year_target = texts[count+1] # i want the value after above tag
+            #print(one_year_target)
+        else:
+            one_year_target = []
+        #print(texts)
+        
+        
     except ConnectionError:
-        price, change = [], []
-    return price, change
+        price, change, volume = [], [], []
+    return price, change, percent, volume, one_year_target
 
 
-Stock = ['safemoon-v2']
-print(real_time_price('safemoon-v2'))
+Stock = ['AAPL']
+print(real_time_price('AAPL'))
