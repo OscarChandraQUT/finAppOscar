d70291689e5d23956ea63f4a6fe8a8dbe20aa2da
OscarChandraQUT <92902350+OscarChandraQUT@users.noreply.github.com>
2022-02-21 16:19:38 +1000

    string_to_number & read_data_ohlc functions

--- finApp3.py
+++ finApp3.py
@@ -31,3 +31,45 @@ def figure_design(ax):
     ax.spines['top'].set_color('#808080')
     ax.spines['left'].set_color('#808080')
     ax.spines['right'].set_color('#808080')
+
+def string_to_number(df, column):
+    if isinstance(df.iloc[0, df.columns.get_loc(column)], str):
+        df[column] = df[column].str.replace(',', '')
+        df[column] = df[column].astype(float)
+    return df
+
+def read_data_ohlc(filename, stock_code, usecols):
+    df = pd.read_csv(filename, header = None, usecols=usecols, names = ['time', stock_code, 'change', 'volume', 'one_year_target'],
+    index_col= 'time', parse_dates=['time'])
+
+    index_with_nan = df.index[df.isnull().any(axis=1)]
+    df.drop(index_with_nan, 0, inplace=True)
+
+    df.index = pd.DatetimeIndex(df.index)
+
+    df = string_to_number(df, stock_code)
+    df = string_to_number(df, 'volume')
+    df = string_to_number(df, 'target')
+
+    latest_info = df.iloc[-1, :]
+    latest_price = str(latest_info[0])
+    latest_change = str(latest_info.iloc[1])
+
+    df_vol = df['volume'].resample('1Min').mean()
+    data = df[stock_code].resample('1Min').ohlc()
+    data['time'] = data.index
+
+    data['time'] = pd.to_datetime(data['time'], format = '%Y-%m-d %H:%M:%S')
+
+    data['MA5'] = data['close'].rolling(5).mean()
+    data['MA10'] = data['close'].rolling(10).mean()
+    data['MA20'] = data['close'].rolling(20).mean()
+
+    data['volume_diff'] = df_vol.diff()
+    data[data['volume_diff'] < 0] = None
+
+    index_with_nan = data.index[data.isnull().any(axis=1)]
+    data.drop(index_with_nan, 0, inplace = True)
+    data.reset_index(drop=True, inplace = True)
+
+    return data, latest_price, latest_change, df['one_year_target'][-1], df['volume'][-1]
