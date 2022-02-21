163b99a698cfbbf8b9a0b7f756776e5608f97758
OscarChandraQUT <92902350+OscarChandraQUT@users.noreply.github.com>
2022-02-21 18:03:29 +1000

    fixed issues: (removed brackets from % change)
    also got correct volume values

--- finApp2.py
+++ finApp2.py
@@ -1,4 +1,5 @@
 # import relevant libraries
+from dataclasses import replace
 import pandas as pd
 import datetime
 import requests
@@ -12,7 +13,20 @@ def web_content_div(web_content, class_path):
     try:
         spans = web_content_div[0].find_all('fin-streamer')
         texts = [span.get_text() for span in spans]
-        #print('texts: ' + str(texts))
+        texts[2] = texts[2].replace('(', '') #cleaning up percentage data
+        texts[2] = texts[2].replace(')', '')
+        #print(texts[2])
+        #print(texts)
+    except IndexError:
+        texts = []
+    return texts
+
+def find_vol(web_content, class_path):
+    web_content_div = web_content.find_all('div', {'class': class_path})
+    try:
+        spans = web_content_div[0].find_all('fin-streamer')
+        texts = [span.get_text() for span in spans]
+        #print(texts)
     except IndexError:
         texts = []
     return texts
@@ -39,9 +53,10 @@ def real_time_price(stock_code):  # get info from url
         else:
             price, change, percent = ['empty'], ['empty'], ['percent']
         #finding volume
-        texts = web_content_div(web_content, 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)') #class containing volume
+        texts = find_vol(web_content, 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)') #class containing volume
         if texts !=[]:
             volume = texts[0]
+            #print('volume: ' + volume) #<<<
         else:
             volume = []
         #find one year target
@@ -60,15 +75,15 @@ def real_time_price(stock_code):  # get info from url
         price, change, volume = [], [], []
     return price, change, percent, volume, one_year_target
 
-
-Stock = ['AAPL']
-print(real_time_price('AAPL'))
+ 
+Stock = ['AAPL', 'GOOG', 'TSLA', 'FB']
+#print(real_time_price('AAPL')) # for debug part 1
 
 #storing to CSV file
 while(True):
     data = []
     col = []
-    time_stamp = datetime.datetime.now() - datetime. timedelta(hours= 15)
+    time_stamp = datetime.datetime.now()
     time_stamp = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
     for stock_code in Stock:
         price, change, percent, volume, one_year_target = real_time_price(stock_code)
@@ -77,10 +92,12 @@ while(True):
         data.extend([percent])
         data.extend([str(volume)])
         data.extend([one_year_target])
-    #print(data[str(volume)])
+    
     col = [time_stamp]
     col.extend(data)
     df = pd.DataFrame(col)
     df = df.T
     df.to_csv(str(time_stamp[0:11]) + 'stock data.csv', mode = 'a', header = False)
     print(col)
+
+    
